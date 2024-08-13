304183301b5d45b5c50abe7a6e298242ba2772c4
Sergey Dvornikov <Serbheys@gmail.com>
2024-08-13 19:09:16 +0300

    Сдача проекта

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import ru.gb.family_tree.view.ConsoleUI;
+import ru.gb.family_tree.view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View view = new ConsoleUI();
+        view.start();
+    }
+}
